67c7c0b98fa4e2e8aca8df8f8e9cc12930c32a1f
dagmawi <doitdagi@gmail.com>
2019-05-08 15:44:28 +0200

    Start script to start the MAS of choice as a main container or not, also with or without gui

---
+++ start-mas.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+#Parameters
+#set main for main container any other string o.w
+
+AGENTS=$1
+IS_MAIN=$2
+#set gui for display any other string o.w
+DISPLAY_GUI=$3
+#Better to modify these values inside this script, than passing the values from the terminal
+HOST_ADDRESS=00.00.00.00 
+HOST_PORT=0000
+
+
+#start agent 
+if [ "$AGENTS" = "bedroom" ]
+then
+     echo "starting bedroom agents"
+     java -cp  ./target/ProbMASSH.jar it.sh.prob.mas.room.bedroom.BedroomMASStarter  $IS_MAIN $DISPLAY_GUI $HOST_ADDRESS $HOST_PORT
+fi
+
+if [ "$AGENTS" = "livingroom" ]
+then 
+	echo "starting living room agent"
+        java -cp  ./target/ProbMASSH.jar it.sh.prob.mas.room.livingroom.LivingroomMASStarter  $IS_MAIN $DISPLAY_GUI $HOST_ADDRESS $HOST_PORT
+fi
+
+if [ "$AGENTS" = "bathroom" ]
+then 
+	echo "starting bathroom agents"
+        java -cp  ./target/ProbMASSH.jar it.sh.prob.mas.room.bathroom.BathroomMASStarter  $IS_MAIN $DISPLAY_GUI $HOST_ADDRESS $HOST_PORT
+fi
+
+if [ "$AGENTS" = "kitchen" ]
+then
+	echo "starting kitchen agents"
+        java -cp  ./target/ProbMASSH.jar it.sh.prob.mas.room.kitchen.KitchenMASStarter  $IS_MAIN $DISPLAY_GUI $HOST_ADDRESS $HOST_PORT
+fi
+
+
+
+
+
+
+
+
+
+
+
